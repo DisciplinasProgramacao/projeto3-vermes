1c7295325b67049a09ece0afff06ea4c0e148d2d
Druitti <gabriel.afa@outlook.com>
2023-10-04 12:11:08 -0300

    Classe Vaga feita

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -2,23 +2,34 @@
 
 public class Vaga {
 
-	private String id;
+	
 	private boolean disponivel;
+	private String id;
 
 	public Vaga(int fila, int numero) {
-		
+		id = Integer.toString(fila) + " - "+ Integer.toString(numero);
+		disponivel = true;
+
 	}
 
 	public boolean estacionar() {
-		
+		if (disponivel) {
+			disponivel = false;
+			return true;
+		}
+		return false;
 	}
 
 	public boolean sair() {
-		
+		if (!disponivel) {
+			disponivel = true;
+			return true;
+		}
+		return false;
 	}
 
 	public boolean disponivel() {
-		
+		return disponivel;
 	}
 
 }
