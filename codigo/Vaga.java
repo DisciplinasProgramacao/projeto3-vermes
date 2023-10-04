fb2de91e088018de98de140e5a7aa21903051f00
Gabriel Ferreira Amaral <64988239+Druitti@users.noreply.github.com>
2023-10-04 12:17:41 -0300

    Voltando o commit

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -2,34 +2,23 @@
 
 public class Vaga {
 
-	
-	private boolean disponivel;
 	private String id;
+	private boolean disponivel;
 
 	public Vaga(int fila, int numero) {
-		id = Integer.toString(fila) + " - "+ Integer.toString(numero);
-		disponivel = true;
-
+		
 	}
 
 	public boolean estacionar() {
-		if (disponivel) {
-			disponivel = false;
-			return true;
-		}
-		return false;
+		
 	}
 
 	public boolean sair() {
-		if (!disponivel) {
-			disponivel = true;
-			return true;
-		}
-		return false;
+		
 	}
 
 	public boolean disponivel() {
-		return disponivel;
+		
 	}
 
 }
