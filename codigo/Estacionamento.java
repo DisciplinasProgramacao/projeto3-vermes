5d7081cf2fdee1fdfa68eb9f177e5a59259fd7a2
Joao Victor Salim <129671396+JvSalim@users.noreply.github.com>
2023-10-04 12:13:42 -0300

    ATULIZAÇAO DIA 04/10 CLASSE ESTACIONAMENTO
    FINALIZAR TOP5 CLIENTES

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -1,51 +1,82 @@
-
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Estacionamento {
 
-	private String nome;
-	private Cliente[] id;
-	private Vaga[] vagas;
-	private int quantFileiras;
-	private int vagasPorFileira;
+    private String nome;
+    private LinkedList<Cliente> clientes;
+    private LinkedList<Vaga> vagas;
+    private int quantFileiras;
+    private int vagasPorFileira;
+    private LinkedList<UsoDeVaga> usos;
 
-	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
-		
-	}
+    public Estacionamento(String nome, int fileiras, int vagasPorFila) {
+        this.nome = nome;
+        this.quantFileiras = fileiras;
+        this.vagasPorFileira = vagasPorFila;
+        this.clientes = new LinkedList<>();
+        this.usos = new LinkedList<>();
+        gerarVagas();
+    }
 
-	public void addVeiculo(Veiculo veiculo, String idCli) {
-		
-	}
+    public void addVeiculo(Veiculo veiculo, String idCli) {
+        Cliente cliente = buscarCliente(idCli);
+        if (cliente != null) {
+            cliente.addVeiculo(veiculo);
+        }
+    }
 
-	public void addCliente(Cliente cliente) {
-		
-	}
+    public void addCliente(Cliente cliente) {
+        if (buscarCliente(cliente.getId()) == null) {
+            clientes.add(cliente);
+        }
+    }
 
-	private void gerarVagas() {
-		
-	}
+    private Cliente buscarCliente(String idCli) {
+        for (Cliente cliente : clientes) {
+            if (cliente != null && cliente.getId().equals(idCli)) {
+                return cliente;
+            }
+        }
+        return null;
+    }
 
-	public void estacionar(String placa) {
-		
-	}
+    private void gerarVagas() {
+        vagas = new LinkedList<>();
+        for (int i = 0; i < quantFileiras; i++) {
+            for (int j = 0; j < vagasPorFileira; j++) {
+                int numeroVaga = i * vagasPorFileira + j + 1;
+                vagas.add(new Vaga(i, numeroVaga));
+            }
+        }
+    }
 
-	public double sair(String placa) {
-		
-	}
+    public void estacionar(String placa) {
+    }
 
-	public double totalArrecadado() {
-		
-	}
+    public double sair(String placa) {
+        
+    }
 
-	public double arrecadacaoNoMes(int mes) {
-		
-	}
+    public double totalArrecadado() {
 
-	public double valorMedioPorUso() {
-		
-	}
+    }
 
-	public String top5Clientes(int mes) {
-		
-	}
+    public double arrecadacaoNoMes(int mes) {
+ 
+    }
 
+    public double valorMedioPorUso() {
+        if (usos.isEmpty()) {
+            return 0.0;
+        }
+        return totalArrecadado() / usos.size();
+    }
+
+    public String top5Clientes(int mes) {
+        // Implementar ainda
+        return null;
+    }
 }
