7634ff42bca526adb505a635a85b79baf5d4adfe
migueltico2 <130784916+migueltico2@users.noreply.github.com>
2023-10-04 12:15:53 -0300

    Update UsoDeVaga.java

--- codigo/UsoDeVaga.java
+++ codigo/UsoDeVaga.java
@@ -1,25 +1,41 @@
-
+import java.time.LocalDateTime;
 
 public class UsoDeVaga {
 
-	private static final double FRACAO_USO = 0.25;
-	private static final double VALOR_FRACAO = 4.0;
-	private static final double VALOR_MAXIMO = 50.0;
-	private Vaga vaga;
-	private LocalDateTime entrada;
-	private LocalDateTime saida;
-	private double valorPago;
+    private static final double FRACAO_USO = 0.25;
+    private static final double VALOR_FRACAO = 4.0;
+    private static final double VALOR_MAXIMO = 50.0;
+    private Vaga vaga;
+    private LocalDateTime entrada;
+    private LocalDateTime saida;
+    private double valorPago;
 
-	public UsoDeVaga(Vaga vaga) {
-		
-	}
+    public UsoDeVaga(Vaga vaga) {
+        this.vaga = vaga;
+        this.entrada = LocalDateTime.now(); 
+    }
 
-	public double sair() {
-		
-	}
+    public void sair() {
+        this.saida = LocalDateTime.now(); 
+        calcularValorPago();
+    }
 
-	public double valorPago() {
-		
-	}
+    public double valorPago() {
+        return valorPago;
+    }
 
+    private void calcularValorPago() {
+        long minutos = calcularDiferencaEmMinutos(entrada, saida);
+        double valorTemp = (minutos / 15) * VALOR_FRACAO;
+        valorPago = valorTemp > VALOR_MAXIMO ? VALOR_MAXIMO : valorTemp;
+    }
+
+    private long calcularDiferencaEmMinutos(LocalDateTime inicio, LocalDateTime fim) {
+        long diferencaEmMinutos = 0;
+        while (inicio.isBefore(fim)) {
+            inicio = inicio.plusMinutes(1);
+            diferencaEmMinutos++;
+        }
+        return diferencaEmMinutos;
+    }
 }
