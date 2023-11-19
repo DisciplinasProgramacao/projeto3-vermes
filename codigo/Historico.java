e337fa7764f643b013a0605823334446fe7f93a4
Lucas Garcia <130924593+lucasgarcia04@users.noreply.github.com>
2023-11-19 11:53:59 -0300

    Classe para pegar o historico de clientes corrigida

---
+++ codigo/Historico.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Historico implements Serializable {
+    private List<Registro> registros;
+
+    public Historico() {
+        registros = new ArrayList<>();
+    }
+
+    public void adicionarRegistro(Registro registro) {
+        registros.add(registro);
+    }
+
+    public List<Registro> getHistorico() {
+        return registros;
+    }
+
+    public List<Registro> getHistoricoPorMes(int mes) {
+        List<Registro> historicoMes = new ArrayList<>();
+        for (Registro registro : registros) {
+            if (registro.getMes() == mes) {
+                historicoMes.add(registro);
+            }
+        }
+        return historicoMes;
+    }
+}
