faadc794c5165f3015abd413eda16831346fa2be
Joao Victor Salim R. G. Trad <129671396+JvSalim@users.noreply.github.com>
2023-11-25 15:51:21 -0300

    Update TestVeiculo.java

--- codigo/TestVeiculo.java
+++ codigo/TestVeiculo.java
@@ -1,9 +1,11 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestVeiculo {
+    
 
     private Veiculo veiculo;
     private Vaga vaga;
@@ -11,47 +13,171 @@ public class TestVeiculo {
     @Before
     public void setUp() {
         veiculo = new Veiculo("ABC1234");
-        
+        vaga = new Vaga(1, 1); // Fornecendo valores válidos para fila e número
+        Servico servico = Servico.MANOBRISTA; // Inicializando o serviço corretamente
+        veiculo.setServico(servico); // Configurando o serviço no veículo
+    }
+    
+    @Test
+    public void testEstacionar() {
+        try {
+            veiculo.estacionar(vaga);
+            assertEquals(1, veiculo.totalDeUsos());
+        } catch (LotadoException | VagaIndisoponivelException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
     }
 
     @Test
-    public void testEstacionar() throws LotadoException, VagaIndisoponivelException {
-        veiculo.estacionar(vaga);
-        assertEquals(1, veiculo.totalDeUsos());
+    public void testSair() {
+        try {
+            veiculo.estacionar(vaga);
+            double valorPago = veiculo.sair();
+            assertTrue(valorPago >= 0);
+        } catch (LotadoException | VagaIndisoponivelException | ServicoNaoExecutadoException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
     }
 
     @Test
-    public void testSair() throws LotadoException, VagaIndisoponivelException, ServicoNaoExecutadoException {
-        veiculo.estacionar(vaga);
-        double valorPago = veiculo.sair();
-        assertTrue(valorPago >= 0);
+    public void testTotalArrecadado() {
+        try {
+            veiculo.estacionar(vaga);
+            veiculo.sair();
+            double valorTotal = veiculo.totalArrecadado();
+            assertTrue(valorTotal >= 0);
+        } catch (LotadoException | VagaIndisoponivelException | ServicoNaoExecutadoException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
     }
 
     @Test
-    public void testTotalArrecadado() throws LotadoException, VagaIndisoponivelException, ServicoNaoExecutadoException {
-        veiculo.estacionar(vaga);
-        veiculo.sair();
-        double valorTotal = veiculo.totalArrecadado();
-        assertTrue(valorTotal >= 0);
+    public void testArrecadadoNoMes() {
+        try {
+            veiculo.estacionar(vaga);
+            veiculo.sair();
+            int mes = 10;
+            double valorArrecadado = veiculo.arrecadadoNoMes(mes);
+            assertTrue(valorArrecadado >= 0);
+        } catch (LotadoException | VagaIndisoponivelException | ServicoNaoExecutadoException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
     }
 
     @Test
-    public void testArrecadadoNoMes() throws LotadoException, VagaIndisoponivelException, ServicoNaoExecutadoException {
-        veiculo.estacionar(vaga);
-        veiculo.sair();
-        int mes = 10;
-        double valorArrecadado = veiculo.arrecadadoNoMes(mes);
-        assertTrue(valorArrecadado >= 0);
-    }
-
-    @Test
-    public void testTotalDeUsos() throws LotadoException, VagaIndisoponivelException {
-        veiculo.estacionar(vaga);
-        assertEquals(1, veiculo.totalDeUsos());
+    public void testTotalDeUsos() {
+        try {
+            veiculo.estacionar(vaga);
+            assertEquals(1, veiculo.totalDeUsos());
+        } catch (LotadoException | VagaIndisoponivelException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
     }
 
     @Test
     public void testGetPlaca() {
         assertEquals("ABC1234", veiculo.getPlaca());
+    }
+
+    @Test
+    public void testGetUsoDeVaga() {
+        try {
+            veiculo.estacionar(vaga);
+            UsoDeVaga uso = veiculo.getUsoDeVaga(0);
+            assertEquals(vaga, uso.getVaga());
+        } catch (LotadoException | VagaIndisoponivelException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
+    }
+}import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestVeiculo {
+    
+
+    private Veiculo veiculo;
+    private Vaga vaga;
+
+    @Before
+    public void setUp() {
+        veiculo = new Veiculo("ABC1234");
+        vaga = new Vaga(1, 1); // Fornecendo valores válidos para fila e número
+        Servico servico = Servico.MANOBRISTA; // Inicializando o serviço corretamente
+        veiculo.setServico(servico); // Configurando o serviço no veículo
+    }
+    
+    @Test
+    public void testEstacionar() {
+        try {
+            veiculo.estacionar(vaga);
+            assertEquals(1, veiculo.totalDeUsos());
+        } catch (LotadoException | VagaIndisoponivelException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
+    }
+
+    @Test
+    public void testSair() {
+        try {
+            veiculo.estacionar(vaga);
+            double valorPago = veiculo.sair();
+            assertTrue(valorPago >= 0);
+        } catch (LotadoException | VagaIndisoponivelException | ServicoNaoExecutadoException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
+    }
+
+    @Test
+    public void testTotalArrecadado() {
+        try {
+            veiculo.estacionar(vaga);
+            veiculo.sair();
+            double valorTotal = veiculo.totalArrecadado();
+            assertTrue(valorTotal >= 0);
+        } catch (LotadoException | VagaIndisoponivelException | ServicoNaoExecutadoException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
+    }
+
+    @Test
+    public void testArrecadadoNoMes() {
+        try {
+            veiculo.estacionar(vaga);
+            veiculo.sair();
+            int mes = 10;
+            double valorArrecadado = veiculo.arrecadadoNoMes(mes);
+            assertTrue(valorArrecadado >= 0);
+        } catch (LotadoException | VagaIndisoponivelException | ServicoNaoExecutadoException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
+    }
+
+    @Test
+    public void testTotalDeUsos() {
+        try {
+            veiculo.estacionar(vaga);
+            assertEquals(1, veiculo.totalDeUsos());
+        } catch (LotadoException | VagaIndisoponivelException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
+    }
+
+    @Test
+    public void testGetPlaca() {
+        assertEquals("ABC1234", veiculo.getPlaca());
+    }
+
+    @Test
+    public void testGetUsoDeVaga() {
+        try {
+            veiculo.estacionar(vaga);
+            UsoDeVaga uso = veiculo.getUsoDeVaga(0);
+            assertEquals(vaga, uso.getVaga());
+        } catch (LotadoException | VagaIndisoponivelException e) {
+            fail("Não deveria lançar exceção aqui.");
+        }
     }
 }
