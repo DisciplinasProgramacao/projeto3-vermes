63ffc2b04cd48f53ddbb57b48cf24bed50e9bb5b
my4wyy <130785414+my4wyy@users.noreply.github.com>
2023-11-25 21:36:36 -0300

    Documentação horista

--- codigo/Horista.java
+++ codigo/Horista.java
@@ -1,18 +1,41 @@
+/**
+ * A classe `Horista` representa um uso de vaga por um cliente do tipo horista em um estacionamento,
+ * implementando a interface Diario.
+ * Um horista possui um tipo de plano, um valor pago e métodos para calcular e obter esse valor.
+ */
 public class Horista extends UsoDeVaga implements Diario {
-    
+
+    /** O tipo de plano associado ao horista, que é sempre do tipo HORISTA. */
     TipoDePlano tipoDePlano = TipoDePlano.HORISTA;
+
+    /** O valor total pago pelo horista. */
     private double valorPago;
 
-    public Horista(Vaga vaga) throws VagaIndisoponivelException{
+    /**
+     * Cria um novo objeto `Horista` associado a uma vaga específica.
+     *
+     * @param vaga A vaga associada ao horista.
+     * @throws VagaIndisponivelException Se a vaga estiver ocupada.
+     */
+    public Horista(Vaga vaga) throws VagaIndisponivelException {
        super(vaga);
     }
 
+    /**
+     * Permite que o cliente do tipo horista estacione a qualquer momento.
+     *
+     * @return true, indicando que o cliente horista pode estacionar a qualquer momento.
+     */
     @Override
     public boolean estacionar() {
         return true;
     }
 
-    
+    /**
+     * Obtém o valor total pago pelo horista.
+     *
+     * @return O valor total pago pelo horista.
+     */
     @Override
     public double valorPago() {
         return valorPago;
