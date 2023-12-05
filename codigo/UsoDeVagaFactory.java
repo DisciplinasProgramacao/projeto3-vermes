e0938f41a1bf8effc4b945c43a334f6094e3af38
my4wyy <130785414+my4wyy@users.noreply.github.com>
2023-12-05 17:09:40 -0300

    Update UsoDeVagaFactory.java

--- codigo/UsoDeVagaFactory.java
+++ codigo/UsoDeVagaFactory.java
@@ -2,7 +2,7 @@ import java.io.Serializable;
 
 public class UsoDeVagaFactory implements Serializable {
 
-    public static UsoDeVaga criarUsoDeVaga(Vaga vaga) throws VagaIndisoponivelException {
+    public static UsoDeVaga criarUsoDeVaga(Vaga vaga)  {
         return new UsoDeVaga(vaga);
     }
 }
