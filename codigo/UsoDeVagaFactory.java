70cf706d64f8d09604dd89261534ec27b756950f
Lucas Garcia <130924593+lucasgarcia04@users.noreply.github.com>
2023-12-12 16:41:03 -0300

    Delete codigo/UsoDeVagaFactory.java

--- codigo/UsoDeVagaFactory.java
+++
@@ -1,8 +0,0 @@
-import java.io.Serializable;
-
-public class UsoDeVagaFactory implements Serializable {
-
-    public static UsoDeVaga criarUsoDeVaga(Vaga vaga) throws VagaIndisoponivelException {
-        return new UsoDeVaga(vaga);
-    }
-}
