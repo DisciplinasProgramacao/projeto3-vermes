daaaa6f6a85ddfd537e167d92e3ccaf7421cc9c9
Lucas Garcia <130924593+lucasgarcia04@users.noreply.github.com>
2023-12-12 16:40:46 -0300

    Delete codigo/VeiculoFactory.java

--- codigo/VeiculoFactory.java
+++
@@ -1,8 +0,0 @@
-import java.io.Serializable;
-
-public class VeiculoFactory implements Serializable {
-
-    public static Veiculo criarVeiculo(String placa) {
-        return new Veiculo(placa);
-    }
-}
